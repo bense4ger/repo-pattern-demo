7028c3bf8ed9fa52b20962f379d9d63f052ac192
Ben Seager <github@benseager.co.uk>
2018-06-08 19:45:42 +0100

    initial commit

---
+++ RepositoryDemo.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryDemo.Web.Models;
+
+namespace RepositoryDemo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
